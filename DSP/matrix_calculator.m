54fa8d69a690b06759d53280954ee38ca422ed02
박지은 <62520045+krapeun@users.noreply.github.com>
2020-07-13 04:17:10 +0900

    Update matrix_calculator.m

--- DSP/matrix_calculator.m
+++ DSP/matrix_calculator.m
@@ -1,19 +1,102 @@
-% 1Week Gaussian 12171788 박지은
+% 2x2 Matrix Simple Calculator 행렬 사칙 연산기
 
-N = 50000;
-R = 100;
-avg = 0;
-stdev = sqrt(2);
+function val = matrix_calculator()
+    mode = input('1. Adder\n2. Subtractor\n3. Multiplier\n4. Divider\nSelect Mode : ');
+    
+    switch(mode)
+        case 1
+            disp('Type two input values');
+            A = input('First input : ');
+            B = input('Second input : ');
+            val = Add_mat(A,B);
+        case 2
+            disp('Type two input values');
+            A = input('First input : ');
+            B = input('Second input : ');
+            val = Sub_mat(A,B);
+        case 3
+            disp('Type two input values');
+            A = input('First input : ');
+            B = input('Second input : ');
+            val = Mul_mat(A,B);
+        case 4
+            disp('Type two input values');
+            A = input('First input : ');
+            B = input('Second input : ');
+            val = Div_mat(A,B);
+        otherwise
+            disp('Invalid Mode Error');
+            return;
+    end
+end
 
-T = randn(1, N);
-G = T*stdev + avg;
+function val = Add_mat(A,B) % Adder
+    [row,col] = size(A);
+    val = zeros(row,col); % 초기화
+    if(size(A)~=size(B)) % A와 B의 크기가 다르면 error
+        disp('Matrices size error! Check your matirces');
+        return;
+    else
+        for k=1:row
+            for kk=1:col
+                val(k,kk) = A(k,kk) + B(k,kk);
+            end
+        end
+    end
+end
 
-[M, X] = hist(G,R);
-resol = X(2) - X(1); % 한 구간의 길이
+function val = Sub_mat(A,B) % Subtractor
+    [row,col] = size(A);
+    val = zeros(row,col); % 초기화
+    if(size(A) ~= size(B)) % A와 B의 크기가 다르면 error
+        disp('Matrices size error! Check your matirces');
+        return;
+    else
+        for k=1:row
+            for kk=1:col
+                val(k,kk) = A(k,kk) - B(k,kk);
+            end
+        end
+    end
+end
 
-pdf = M/N/resol;
-cdf = cumsum(pdf*resol);
+function val = Mul_mat(A,B) % Multiplier
+    [rowOfA,columeOfA]=size(A);
+    [rowOfB,columeOfB]=size(B);
+    val = zeros(rowOfA,columeOfB); % 초기화
+    if(columeOfA ~= rowOfB) % A의 열과 B의 행의 크기가 다르면 error
+        disp('Matrices size error! Check your matirces');
+        return;
+    else
+        for k=1:rowOfA
+            for kk=1:columeOfB
+                for temp=1:columeOfA
+                    val(k,kk) = val(k,kk) + A(k,temp) * B(temp,kk);
+                end
+            end
+        end
+    end
+end
 
-figure(1); hold on;
-bar(X, pdf); plot(X, cdf);
-title('Gaussian PDF/CDF');
+function val = Div_mat(A,B) % Divider
+    [rowOfA,columeOfA]=size(A);
+    [rowOfB,columeOfB]=size(B);
+    val = zeros(rowOfA,columeOfB); % 초기화
+    if(columeOfA ~= rowOfB) % A의 열과 B의 행의 크기가 다르면 error
+        disp('Matrices size error! Check your matirces');
+        %return;
+    elseif(det(B)==0) % B가 invertible하지 않으면 error
+        disp('Matrices must be invertible!');
+        %return;
+    else
+        C = inv(B);
+        [rowOfC,columeOfC]=size(C);
+        for k=1:rowOfA
+            for kk=1:columeOfC
+                for temp=1:columeOfA
+                    val(k,kk) = val(k,kk) + A(k,temp) * C(temp,kk);
+                end
+            end
+        end
+    end
+end
